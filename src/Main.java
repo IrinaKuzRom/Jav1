3ee11f9c012c68d561450beff3622d4b2e4282fa
IrinaKuzRom <73582147+IrinaKuzRom@users.noreply.github.com>
2022-07-02 03:29:19 +1100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         int tax = incore * 6 / 100; // налог 6% на доходы
         int tax2 = (incore - spending) * 15 / 100; // налог 15% на разницу доходов и расходов
-        int spices = 10; // ложек специй
+        
 
         System.out.println("Ваш налог на первой системе: " + tax + "рублей");
         System.out.println("Ваш налог на второй системе: " +tax2 + "рублей");
